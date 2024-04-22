49356aae03231ce2d520a6df53978a859bf09b15
Shapovalov Ilia <38822038+ShapovalovIlia@users.noreply.github.com>
2024-04-22 20:11:06 +0300

    final changes

--- parser/src/main/java/Main.java
+++ parser/src/main/java/Main.java
@@ -2,34 +2,39 @@ import entity.Info;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
         final Parser parser = new Parser();
+        if (args[0] == null) {
+            System.err.println("Incorrect path");
+            return;
+        }
         String path = args[0];
 
+        Info info;
         try {
-            Info info = parser.parse(path);
-
-            for (HashMap.Entry<String, Long> entry : info.carrierMinFlightTime().entrySet()) {
-                long timeDiff = entry.getValue();
-                long hourDiff = timeDiff / (1000 * 60 * 60);
-                long minuteDiff = (timeDiff / (1000 * 60)) % 60;
-
-                String formattedHours = String.format("%02d", hourDiff);
-                String formattedMinutes = String.format("%02d", minuteDiff);
-                System.out.println(
-                    "Carrier - " + entry.getKey() + " | Minimum flight time - "
-                    + formattedHours + ":" + formattedMinutes
-                );
-            }
-
-            System.out.println(
-                "Difference between median and average price: " + (info.medianAvgPriceDiff())
-            );
+            info = parser.parse(path);
         } catch (IOException | ParseException e) {
             System.err.println(e.getMessage());
+            return;
         }
+
+        for (var entry : info.carrierMinFlightTime().entrySet()) {
+            long timeDiff = entry.getValue();
+            long hourDiff = timeDiff / (1000 * 60 * 60);
+            long minuteDiff = (timeDiff / (1000 * 60)) % 60;
+
+            String formattedHours = String.format("%02d", hourDiff);
+            String formattedMinutes = String.format("%02d", minuteDiff);
+            System.out.printf(
+                    "Carrier - %s | Minimum flight time - %s:%s%n",
+                    entry.getKey(),  formattedHours, formattedMinutes
+            );
+        }
+
+        System.out.println(
+            "Difference between median and average price: " + Math.abs(info.medianAvgPriceDiff())
+        );
     }
 }
